21c62e6c5307443df2372c399841e1aa4b72199c
nehal <nehalelsamoly123@gmail.com>
2025-04-15 10:21:55 +0200

    edits

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -10,30 +10,36 @@ class CustomButton extends StatelessWidget {
     super.key,
     this.onTap,
     required this.title,
+     this.color,
+    this.height,
+    this.textColor,
   });
  final  void Function()? onTap;
  final String title;
+ final Color ?textColor;
+ final double ?height;
+ final Color ?color;
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
    //   borderRadius: BorderRadius.circular(getSize(context) / 12),
       child: Container(
-height: 30.h,
+height: height??30.h,
        // width: double.infinity,
         alignment: Alignment.center,
         padding: EdgeInsets.symmetric(horizontal: 50.w),
         decoration: BoxDecoration(
-          color: AppColors.transparent,
+          color: color??AppColors.transparent,
           borderRadius: BorderRadius.circular(8.r),
           border: Border.all(width: 1.0,color: AppColors.white)
         ),
         child: Text(
           title,
           style: TextStyle(
-            color: AppColors.white,
+            color:textColor ??AppColors.white,
             fontFamily: AppStrings.fontFamily,
-            fontWeight: FontWeight.w400,
+            fontWeight:FontWeight.w400,
             fontSize: 14.sp
           ),
         ),
