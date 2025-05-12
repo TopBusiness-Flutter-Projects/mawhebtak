ad5c12f4cf3ddcc3f345f472f08d6a33c57481a4
ahmedelsapagh <semicolonsales@gmmail.com>
2025-05-12 13:00:10 +0300

    Refactor progress dialog implementation and add custom loading indicator

---
+++ lib/core/utils/custom_loadingindicator.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import '../exports.dart';
+
+class CustomLoadingIndicator extends StatefulWidget {
+  const CustomLoadingIndicator({super.key, this.withLogo = true});
+  final bool withLogo;
+
+  @override
+  State<CustomLoadingIndicator> createState() => _CustomLoadingIndicatorState();
+}
+
+class _CustomLoadingIndicatorState extends State<CustomLoadingIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.withLogo
+        ? Stack(
+            alignment: Alignment.center,
+            children: [
+              // // Platform specific loading indicator in the background
+              // Platform.isIOS
+              //     ? const CupertinoActivityIndicator(
+              //         animating: true,
+              //         radius: 30,
+              //       )
+              //     :
+              SizedBox(
+                height: 55.h, // Increased size
+                width: 55.h, // Increased size
+                child: CircularProgressIndicator(
+                  color: AppColors.primary,
+                  strokeWidth: 3, // Slightly increased thickness
+                ),
+              ),
+              // Rotating logo in the center
+              RotationTransition(
+                turns: _controller,
+                child: Image.asset(
+                  ImageAssets.logoImage,
+                  height: 50.h,
+                  width: 50.h,
+                ),
+              ),
+            ],
+          )
+        : Platform.isIOS
+            ? const CupertinoActivityIndicator(
+                animating: true,
+                // radius: 30,
+              )
+            : CircularProgressIndicator(
+                color: AppColors.primary,
+                strokeWidth: 3, // Slightly increased thickness
+              );
+  }
+}
