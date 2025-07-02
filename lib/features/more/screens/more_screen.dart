bf224f7990dd68833a442738074b1c21acb8f615
ahmedelsapagh <semicolonsales@gmmail.com>
2025-07-02 11:39:43 +0300

    Refactor import paths for MoreScreen and its dependencies3

--- lib/features/more/screens/more_screen.dart
+++ lib/features/more/screens/more_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mawhebtak/config/routes/app_routes.dart';
 import 'package:mawhebtak/features/home/screens/widgets/custom_app_bar_row.dart';
-import 'package:mawhebtak/features/more/cubit/more_cubit.dart';
-import 'package:mawhebtak/features/more/screens/widget/custom_logout_dialog.dart';
 import '../../../core/exports.dart';
 import '../../../core/preferences/preferences.dart';
 import '../../../core/utils/check_login.dart';
 import '../../events/screens/details_event_screen.dart';
+import '../cubit/more_cubit.dart';
+import 'widget/custom_logout_dialog.dart';
 
 class MoreScreen extends StatefulWidget {
   const MoreScreen({super.key});
