96d1264dcdde7ee2a6237570cb7bec2f29a17ed6
ThanaAdel <swaadl971@gmail.com>
2025-07-08 14:52:17 -0700

    image in home screen

--- lib/features/home/screens/widgets/youtube_player_screen.dart
+++ lib/features/home/screens/widgets/youtube_player_screen.dart
@@ -1,36 +1,36 @@
-
-import 'package:mawhebtak/core/exports.dart';
-import 'package:youtube_quality_player/youtube_quality_player.dart';
-
-
-
-class YouTubePlayerScreen extends StatefulWidget {
-  @override
-  _YouTubePlayerScreenState createState() => _YouTubePlayerScreenState();
-}
-
-class _YouTubePlayerScreenState extends State<YouTubePlayerScreen> {
-  String currentVideoUrl = 'https://www.youtube.com/watch?v=Eq6X_nNUbDo';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('YouTube Quality Player'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 250,
-            child: YQPlayer(
-              videoLink: currentVideoUrl,
-              primaryColor: Colors.blue,
-              secondaryColor: Colors.redAccent,
-            ),
-          ),
-          SizedBox(height: 20),
-        ],
-      ),
-    );
-  }
-}
+//
+// import 'package:mawhebtak/core/exports.dart';
+// import 'package:youtube_quality_player/youtube_quality_player.dart';
+//
+//
+//
+// class YouTubePlayerScreen extends StatefulWidget {
+//   @override
+//   _YouTubePlayerScreenState createState() => _YouTubePlayerScreenState();
+// }
+//
+// class _YouTubePlayerScreenState extends State<YouTubePlayerScreen> {
+//   String currentVideoUrl = 'https://www.youtube.com/watch?v=Eq6X_nNUbDo';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('YouTube Quality Player'),
+//       ),
+//       body: Column(
+//         children: [
+//           Container(
+//             height: 250,
+//             child: YQPlayer(
+//               videoLink: currentVideoUrl,
+//               primaryColor: Colors.blue,
+//               secondaryColor: Colors.redAccent,
+//             ),
+//           ),
+//           SizedBox(height: 20),
+//         ],
+//       ),
+//     );
+//   }
+// }
