840a2b892150fff6051f5e75fcfdadcaa2d8e819
ThanaAdel <swaadl971@gmail.com>
2025-07-08 14:51:54 -0700

    image in home screen

--- lib/features/home/screens/widgets/youtube_player_screen.dart
+++ lib/features/home/screens/widgets/youtube_player_screen.dart
@@ -2,6 +2,8 @@
 import 'package:mawhebtak/core/exports.dart';
 import 'package:youtube_quality_player/youtube_quality_player.dart';
 
+
+
 class YouTubePlayerScreen extends StatefulWidget {
   @override
   _YouTubePlayerScreenState createState() => _YouTubePlayerScreenState();
